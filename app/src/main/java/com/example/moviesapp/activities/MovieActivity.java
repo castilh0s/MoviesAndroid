a52c2cccc562a9e491c8b82c526e7981e24d9b75
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-03 21:50:17 -0300

    :sparkles: Texto muda de acordo com filme

--- app/src/main/java/com/example/moviesapp/activities/MovieActivity.java
+++ app/src/main/java/com/example/moviesapp/activities/MovieActivity.java
@@ -6,16 +6,33 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.moviesapp.R;
+import com.example.moviesapp.repositories.Movie;
 
 public class MovieActivity extends AppCompatActivity {
+
+    TextView textViewTitle;
+    TextView textViewDate;
+    TextView textViewSinopsis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
         addListenerOnWatchButton();
+
+        textViewTitle = findViewById(R.id.movieTitleText);
+        textViewDate = findViewById(R.id.dateText);
+        textViewSinopsis = findViewById(R.id.sinopsisText);
+
+        Bundle data = getIntent().getExtras();
+        Movie movie = (Movie) data.getSerializable("MOVIE");
+
+        textViewTitle.setText(movie.getTitle());
+        textViewDate.setText(movie.getDate());
+        textViewSinopsis.setText(movie.getSinopsis());
     }
 
     public void addListenerOnWatchButton() {
--- app/src/main/java/com/example/moviesapp/activities/MoviesListActivity.java
+++ app/src/main/java/com/example/moviesapp/activities/MoviesListActivity.java
@@ -1,15 +1,86 @@
 package com.example.moviesapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.moviesapp.R;
+import com.example.moviesapp.repositories.Movie;
 
 public class MoviesListActivity extends AppCompatActivity {
+
+    private Button buttonIronMan;
+    private Button buttonForrestGump;
+    private Button buttonTheIncredibles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_list);
+
+        final Intent movieIntent = new Intent(
+                getApplicationContext(),
+                MovieActivity.class
+        );
+
+        buttonIronMan = findViewById(R.id.buttonIronMan);
+        buttonForrestGump = findViewById(R.id.buttonForrestGump);
+        buttonTheIncredibles = findViewById(R.id.buttonTheIncredibles);
+
+        buttonIronMan.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Movie ironMan = new Movie(
+                                null,
+                                null,
+                                "Iron Man",
+                                "30/04/2008",
+                                "Tony Stark (Robert Downey Jr.) é um industrial bilionário, que também é um brilhante inventor. Ao ser sequestrado ele é obrigado por terroristas a construir uma arma devastadora mas, ao invés disto, constrói uma armadura de alta tecnologia que permite que fuja de seu cativeiro. A partir de então ele passa a usá-la para combater o crime, sob o alter-ego do Homem de Ferro."
+                        );
+
+                        movieIntent.putExtra("MOVIE", ironMan);
+                        startActivity(movieIntent);
+                    }
+                }
+        );
+
+        buttonForrestGump.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Movie forrestGump = new Movie(
+                                null,
+                                null,
+                                "Forrest Gump",
+                                "06/07/1994",
+                                "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções. Por obra do acaso, ele consegue participar de momentos cruciais, como a Guerra do Vietnã e Watergate, mas continua pensando no seu amor de infância, Jenny Curran."
+                        );
+
+                        movieIntent.putExtra("MOVIE", forrestGump);
+                        startActivity(movieIntent);
+                    }
+                }
+        );
+
+        buttonTheIncredibles.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Movie theIncredibles = new Movie(
+                          null,
+                          null,
+                          "The Incredibles",
+                          "10/12/2004",
+                          "Roberto Pêra (Craig T. Nelson) já foi o maior herói do planeta, salvando vidas e combatendo o mal todos os dias sob o codinome Sr. Incrível. Porém, após salvar um homem de se suicidar, ele é processado e condenado na Justiça. Uma série de processos seguintes faz com que o Governo tenha que desembolsar uma alta quantia para pagar as indenizações, o que faz com que a opinião pública se volte contra os super-heróis. Em reconhecimento aos serviços prestados, o Governo faz a eles uma oferta: que levem suas vidas como pessoas normais, sem demonstrar que possuem superpoderes, recebendo em troca uma pensão anual. Quinze anos depois, Roberto leva uma vida pacata ao lado de sua esposa Helen (Holly Hunter), que foi a super-heroína Mulher-Elástica, e seus três filhos. Roberto agora trabalha em uma seguradora e luta para combater o tédio da vida de casado e o peso extra. Com vontade de retomar a vida de herói, ele tem a grande chance quando surge um comunicado misterioso, que o convida para uma missão secreta em uma ilha remota."
+                        );
+
+                        movieIntent.putExtra("MOVIE", theIncredibles);
+                        startActivity(movieIntent);
+                    }
+                }
+        );
     }
 }
