de6fb94489ee59274e736a09c1029f733553ac5c
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-17 20:30:52 -0300

    :sparkles: Novas requisições

--- app/src/main/java/com/example/moviesapp/services/TheMovieDBService.java
+++ app/src/main/java/com/example/moviesapp/services/TheMovieDBService.java
@@ -1,5 +1,7 @@
 package com.example.moviesapp.services;
 
+import com.example.moviesapp.models.Credits;
+import com.example.moviesapp.models.GenreList;
 import com.example.moviesapp.models.MoviesList;
 
 import retrofit2.Call;
@@ -7,9 +9,17 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface TheMovieDBService {
-    @GET("discover/movie?api_key=11518df004464f136f1ea9018d4db6d1&sort_by=popularity.desc")
+    String API_KEY = "11518df004464f136f1ea9018d4db6d1";
+
+    @GET("discover/movie?sort_by=popularity.desc&api_key=" + API_KEY)
     Call<MoviesList> listMovies();
 
-    @GET("/search/movie?api_key=11518df004464f136f1ea9018d4db6d1&query=${movie}")
+    @GET("/search/movie?query={movie}&api_key=" + API_KEY)
     Call<MoviesList> searchMovie(@Path("movie") String movie);
+
+    @GET("/genre/movie/list?api_key=" + API_KEY)
+    Call<GenreList> getGenreList();
+
+    @GET("/movie/{movie_id}/credits?api_key=" + API_KEY)
+    Call<Credits> getCredits(@Path("movie_id") String movie_id);
 }
