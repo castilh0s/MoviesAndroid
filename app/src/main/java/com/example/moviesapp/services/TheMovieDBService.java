5abd6163348aba15de8ec78a39b6a4d4c8ad2816
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-17 00:26:44 -0300

    :sparkles: Busca de filmes

--- app/src/main/java/com/example/moviesapp/services/TheMovieDBService.java
+++ app/src/main/java/com/example/moviesapp/services/TheMovieDBService.java
@@ -1,11 +1,15 @@
 package com.example.moviesapp.services;
 
-import com.example.moviesapp.services.models.MoviesList;
+import com.example.moviesapp.models.MoviesList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface TheMovieDBService {
     @GET("discover/movie?api_key=11518df004464f136f1ea9018d4db6d1&sort_by=popularity.desc")
     Call<MoviesList> listMovies();
+
+    @GET("/search/movie?api_key=11518df004464f136f1ea9018d4db6d1&query=${movie}")
+    Call<MoviesList> searchMovie(@Path("movie") String movie);
 }
