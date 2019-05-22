3b17ba096779c903b0c480fb86c0be2779c0e376
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-22 16:47:32 -0300

    :sparkles: Enviando os dados para os adapters

--- app/src/main/java/com/example/moviesapp/activities/MovieActivity.java
+++ app/src/main/java/com/example/moviesapp/activities/MovieActivity.java
@@ -2,19 +2,38 @@ package com.example.moviesapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.moviesapp.R;
+import com.example.moviesapp.adapter.CastAdapter;
+import com.example.moviesapp.adapter.CrewAdapter;
+import com.example.moviesapp.models.Cast;
+import com.example.moviesapp.models.Credits;
+import com.example.moviesapp.models.Crew;
 import com.example.moviesapp.models.Movie;
+import com.example.moviesapp.services.TheMovieDB;
+import com.example.moviesapp.services.TheMovieDBService;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MovieActivity extends AppCompatActivity {
 
-    TextView textViewTitle;
-    TextView textViewDate;
-    TextView textViewSinopsis;
-    ImageView imageViewPoster;
+    private static final String TAG = MovieActivity.class.getSimpleName();
+    private TextView textViewTitle;
+    private TextView textViewDate;
+    private TextView textViewSinopsis;
+    private ImageView imageViewPoster;
+    private RecyclerView recyclerViewCast;
+    private RecyclerView recyclerViewCrew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +52,34 @@ public class MovieActivity extends AppCompatActivity {
         textViewDate.setText(movie.getReleaseDate());
         textViewSinopsis.setText(movie.getOverview());
         Picasso.get().load("https://image.tmdb.org/t/p/w600_and_h900_bestv2/" + movie.getPosterPath()).into(imageViewPoster);
+
+        LinearLayoutManager layoutManagerCast = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager layoutManagerCrew = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerViewCast = findViewById(R.id.castRecyclerView);
+        recyclerViewCast.setLayoutManager(layoutManagerCast);
+
+        recyclerViewCrew = findViewById(R.id.crewRecyclerView);
+        recyclerViewCrew.setLayoutManager(layoutManagerCrew);
+
+        TheMovieDBService apiService = TheMovieDB.getRetrofit().create(TheMovieDBService.class);
+        Call <Credits> call = apiService.getCredits(movie.getId().toString());
+        call.enqueue(new Callback<Credits>() {
+            @Override
+            public void onResponse(Call<Credits> call, Response<Credits> response) {
+                Integer statusCode = response.code();
+                final Credits credits = response.body();
+                final List<Cast> castList = credits.getCast();
+                final List<Crew> crewList = credits.getCrew();
+
+                recyclerViewCast.setAdapter(new CastAdapter(castList, R.layout.adapter_credits, getApplicationContext()));
+                recyclerViewCrew.setAdapter(new CrewAdapter(crewList, R.layout.adapter_credits, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<Credits> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+        });
     }
 }
